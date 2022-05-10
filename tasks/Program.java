ae92582078918bb9b7629f258020e4a3a87d63cd
IVKornienko <Iv@n2784!>
2022-05-10 20:27:58 +0300

    Запись файла

--- tasks/Program.java
+++ tasks/Program.java
@@ -1,4 +1,16 @@
 package tasks;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        FileWriter fw = new FileWriter("input.txt");
+        fw.write(str);
+        fw.close();
+    }
 }
