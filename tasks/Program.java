1feceb6f53d08884757d2115124e7a7b796aee8b
IVKornienko <Iv@n2784!>
2022-05-10 20:21:27 +0300

    Чтение файла

--- tasks/Program.java
+++ tasks/Program.java
@@ -1,4 +1,15 @@
 package tasks;
 
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader("input.txt");
+        Scanner sc = new Scanner(fr);
+        String str = sc.nextLine();
+        fr.close();
+
+        System.out.println(str);
+    }
 }
