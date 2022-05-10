5444eb03247da71188a3b40f2ea40a3ec3ca2689
IVKornienko <Iv@n2784!>
2022-05-10 19:24:50 +0300

    Добавлен новый пкет, интерфейс и классы

---
+++ transport/MilitaryTransport.java
@@ -0,0 +1,13 @@
+package transport;
+
+public class MilitaryTransport implements Transport {
+
+}
+
+class Tank extends MilitaryTransport {
+
+}
+
+class Fighter extends MilitaryTransport {
+
+}
---
+++ transport/SocialTransport.java
@@ -0,0 +1,13 @@
+package transport;
+
+public class SocialTransport implements Transport {
+
+}
+
+class Bus extends SocialTransport {
+
+}
+class Troll extends SocialTransport {
+
+}
+
